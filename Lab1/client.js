9474e57fecfccf82f70d01eebb24cd5762afb916
Martin Bergstedt <marbe943@student.liu.se>
2018-01-18 16:30:27 +0100

    working on sign in

--- Lab1/client.js
+++ Lab1/client.js
@@ -1,11 +1,20 @@
 var welcomeView;
-var loginView;
+var profileView;
+
+function loginClicked(){
+    console.log("Mordin");
+}
+
 
 displayView = function(){
  // the code required to display a view
     document.getElementById("body").innerHTML = welcomeView.innerHTML;
-
 };
+
+displayProfileView = function(){
+    document.getElementById("body").innerHTML = profileView.innerHTML;
+};
+
 window.onload = function(){
  //code that is executed as the page is loaded.
  //You shall put your own custom code here.
@@ -14,5 +23,6 @@ window.onload = function(){
 
     //Setup variables
     welcomeView = document.getElementById("welcomeview");
+    profileView = document.getElementById("profileview");
     displayView();
 };
