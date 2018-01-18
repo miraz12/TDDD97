0f2ce3b3fcf36941411d9ccd866d3773ac775e6e
Anton Christoffersson <anton_christoffersson@hotmail.com>
2018-01-18 15:47:32 +0100

    Added view loading

--- Lab1/client.js
+++ Lab1/client.js
@@ -1,9 +1,17 @@
+var welcomeView;
+
 displayView = function(){
  // the code required to display a view
+    document.getElementById("body").innerHTML = welcomeView.innerHTML;
+
 };
 window.onload = function(){
  //code that is executed as the page is loaded.
  //You shall put your own custom code here.
  //window.alert() is not allowed to be used in your implementation.
- window.alert("Hello TDDD97!");
+ //window.alert("Hello TDDD97!");
+
+    //Setup variables
+    welcomeView = document.getElementById("welcomeview");
+    displayView();
 };
