16a0d47034833417c5158de573b25f494f65dbc9
Shaggy <anton_christoffersson@hotmail.com>
2018-01-29 17:28:14 +0100

    Started working on lab 2

--- lab2/server.py
+++ lab2/server.py
@@ -2,9 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Hello World!"
+def start():
+    return app.send_static_file("client.html")
 
 if __name__ == '__main__':
-	app.run(127.0.01, 5001)
+	app.run()
 
---
+++ lab2/static/client.js
@@ -0,0 +1,267 @@
+var welcomeView;
+var profileView;
+var userInfo;
+
+test = function(){
+    //console.log("Test");
+}
+
+loginClicked = function(){
+    //console.log("loginClicked");
+    var form = document.getElementById("loginForm");
+    var username = form.elements["inputEmail"].value;
+    var password = form.elements["inputPassword"].value;
+    
+    var returnMessage = serverstub.signIn(username, password);
+    var token = returnMessage.data;
+
+    
+    //console.log(returnMessage.message);
+    //console.log("token is " + token);
+    document.getElementById("singIn-error").innerHTML = returnMessage.message;
+    if(returnMessage.success === true)
+    {
+        localStorage.setItem("token", token);
+        displayView();
+    }
+
+}
+
+logoutClicked = function() {
+     localStorage.removeItem("token");
+     displayView();
+}
+
+loadPersonalInfo = function(){
+    //console.log(userInfo);
+    document.getElementById("nameLabel").innerText = userInfo.firstname;
+    document.getElementById("familynameLabel").innerText = userInfo.familyname;
+    document.getElementById("emailLabel").innerText = userInfo.email;
+    document.getElementById("genderLabel").innerText = userInfo.gender;
+    document.getElementById("cityLabel").innerText = userInfo.city;
+    document.getElementById("countryLabel").innerText = userInfo.country;
+}
+
+
+displayView = function(){
+ // the code required to display a view
+    var token = localStorage.getItem("token");
+    if(token !== null)
+    {
+        document.getElementById("body").innerHTML = profileView.innerHTML;
+        userInfo = serverstub.getUserDataByToken(localStorage.getItem("token")).data;
+        openTab("homeTab"); //Hard code is best code
+    }
+    else
+    {
+        document.getElementById("body").innerHTML = welcomeView.innerHTML;
+    }
+};
+
+displayProfileView = function(){
+    document.getElementById("body").innerHTML = profileView.innerHTML;
+};
+
+window.onload = function(){
+ //code that is executed as the page is loaded.
+ //You shall put your own custom code here.
+ //window.alert() is not allowed to be used in your implementation.
+ //window.alert("Hello TDDD97!");
+
+    //Setup variables
+    welcomeView = document.getElementById("welcomeview");
+    profileView = document.getElementById("profileview");
+    displayView();
+};
+
+signUpClicked = function () {
+    //console.log("signUpClicked");
+    var from = document.getElementById("signupForm");
+    var name = from.elements["Name"];
+    var familyname = from.elements["Family"];
+    var gender = from.elements["Gender"];
+    var city = from.elements["City"];
+    var country = from.elements["Country"];
+    var email = from.elements["Email"];
+    var password = from.elements["Password"];
+    var rptPassword = from.elements["RptPassword"];
+
+    if(password.value !== rptPassword.value)
+    {
+        document.getElementById("singUp-error").innerHTML = "Passwords not matching!";
+    }
+    else    //Signup
+    {
+        var newUser = {
+                "email":email.value,
+                "password":password.value,
+                "firstname":name.value,
+                "familyname":familyname.value,
+                "gender":gender.value,
+                "city":city.value,
+                "country":country.value
+        }
+
+        var returnMessage = serverstub.signUp(newUser);
+        document.getElementById("singUp-error").innerHTML = returnMessage.message;
+
+        var returnMessage = serverstub.signIn(newUser.email, newUser.password);
+        
+        console.log(returnMessage);
+        if(returnMessage.success === true)
+        {
+            localStorage.setItem("token", returnMessage.data);
+            displayView();
+        }
+    }
+}
+
+openTab = function(tab){
+    //Hide all tabs
+    document.getElementById("homeTab").style.display = "none";
+    document.getElementById("browseTab").style.display = "none";
+    document.getElementById("accountTab").style.display = "none";
+
+    //Set button color to be no active
+    document.getElementById("homeTabButton").style.backgroundColor = "inherit";
+    document.getElementById("browseTabButton").style.backgroundColor = "inherit";
+    document.getElementById("accountTabButton").style.backgroundColor = "inherit";
+    
+
+    //Activate the right view aswell as the right button
+    if(tab === "homeTab"){
+        loadPersonalInfo();
+        document.getElementById("homeTab").style.display = "block";
+        document.getElementById("homeTabButton").style.backgroundColor = "#ccc";
+    }
+    else if(tab === "browseTab"){
+        document.getElementById("browseEmailLabel").innerText = "null";
+        document.getElementById("browseTab").style.display = "block";
+        document.getElementById("browseTabButton").style.backgroundColor = "#ccc";
+    }
+    else if(tab === "accountTab"){
+        document.getElementById("accountTab").style.display = "block";     
+        document.getElementById("accountTabButton").style.backgroundColor = "#ccc";
+    }
+}
+
+//Click functions for meny
+homePressed = function(){
+    //console.log("homePressed");
+    openTab("homeTab");
+    
+}
+
+browsePressed = function(){
+    //console.log("browsePressed");
+    openTab("browseTab");
+    
+}
+
+accountPressed = function(){
+    //console.log("accountPressed");    
+    openTab("accountTab");
+    
+}
+
+changePasswordClicked = function(){
+    //console.log("changePWPressed");
+    var form = document.getElementById("changePasswordForm");
+    var password = form.elements["Password"].value;
+    var rptPassword = form.elements["RptPassword"].value;
+    var oldPassword = form.elements["oldPassword"].value;
+
+    if(password.value !== rptPassword.value)
+    {
+        //console.log("Error, passwords doesn't match");
+        document.getElementById("changePW-error").innerHTML = "Error, passwords doesn't match";
+        
+    }
+    else{
+        var returnMessage = serverstub.changePassword(localStorage.getItem("token"), oldPassword, password);
+        //console.log(returnMessage);
+        document.getElementById("changePW-error").innerHTML = returnMessage.message; //DETTA ÄR KNAS MEN DET FUNKAR (fel return message)
+        
+    }
+}
+
+refreshWallClicked = function(){
+    //console.log("refresh wall clicked");
+    var posts = serverstub.getUserMessagesByToken(localStorage.getItem("token")).data;
+    //console.log(posts);
+    
+    var wallDiv = document.getElementById("postsDiv");
+    wallDiv.innerHTML = "";
+    for(var i = 0; i < posts.length; i++){
+        var tmpDiv = document.createElement("div");
+        tmpDiv.setAttribute("class", "wallPosts");
+        tmpDiv.innerText = posts[i].writer + ": " + posts[i].content;            
+        wallDiv.appendChild(tmpDiv);
+    }
+}
+
+postClicked = function(){
+    //console.log("post on wall clicked");
+    var text = document.getElementById("postInput").value;
+    document.getElementById("postInput").value = ""; //Clear textarea
+    //console.log(text);    
+    var retM = serverstub.postMessage(localStorage.getItem("token"),text ,userInfo.email);
+    refreshWallClicked();
+    //console.log(retM.message);
+
+}
+
+getUserPage = function(){
+    var returnMessage = serverstub.getUserDataByEmail(localStorage.getItem("token"), document.getElementById("userSearch").value);
+    document.getElementById("searchUser-error").innerHTML = "";
+    //console.log("user search clicked");
+    
+
+    if(returnMessage.success === false){
+        document.getElementById("searchUser-error").innerHTML = returnMessage.message;
+        //console.log("error here");
+    }
+    else{
+        //console.log(returnMessage.data);  
+        var userData = returnMessage.data;
+        document.getElementById("browseNameLabel").innerText = userData.firstname;
+        document.getElementById("browseFamilynameLabel").innerText = userData.familyname;
+        document.getElementById("browseEmailLabel").innerText = userData.email;
+        document.getElementById("browseGenderLabel").innerText = userData.gender;
+        document.getElementById("browseCityLabel").innerText = userData.city;
+        document.getElementById("browseCountryLabel").innerText = userData.country;        
+    }
+}
+
+postToUserClicked = function(){
+    //console.log("post on users wall clicked");
+    var text = document.getElementById("postUserInput").value;
+    document.getElementById("postUserInput").value = ""; //Clear textarea
+    //console.log(text);
+    
+    var userEmail = document.getElementById("browseEmailLabel").innerText;
+    var retM = serverstub.postMessage(localStorage.getItem("token"),text ,userEmail);
+    refreshUserWallClicked(); //Not clicked but still reused
+    //console.log(retM.message);
+}
+
+refreshUserWallClicked = function(){
+    //console.log("refresh user wall clicked");
+    var userEmail = document.getElementById("browseEmailLabel").innerText;
+    if(userEmail === "null"){ //Comparing against content of the div tag is maybe not that good (since we shouldn't display "null" in the first place)
+        document.getElementById("searchUser-error").innerHTML = "No selected user"; //Maybe don't have any output 
+    }
+    else{
+        var posts = serverstub.getUserMessagesByEmail(localStorage.getItem("token"),userEmail).data;
+        //console.log(posts);
+
+        var wallDiv = document.getElementById("userPostsDiv");
+        wallDiv.innerHTML = "";
+        for(var i = 0; i < posts.length; i++){
+            var tmpDiv = document.createElement("div");
+            tmpDiv.setAttribute("class", "wallPosts");    
+            tmpDiv.innerText = posts[i].writer + ": " + posts[i].content;            
+            wallDiv.appendChild(tmpDiv);
+        }
+    }    
+}
